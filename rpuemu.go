3e8a08a130a65cbc3e7cbfb34c38c149833b251a
Alexander Surma <surma@zib.de>
2010-06-30 10:38:12 +0200

    Some more stuff, gonna start over with literate though

--- rpuemu.go
+++ rpuemu.go
@@ -8,21 +8,35 @@ import (
 )
 
 type RPU struct {
-	Memory []byte
-	IP uint16
-	Accumulator byte
+	memory []byte
+	ip uint16
+	accumulator Bit
 }
 
+type Bit byte
+type Op Bit
+
+const (
+    LOAD = 0
+    STORE = 1
+)
+
+type Phase byte
+const (
+    LOAD1 = 0
+    LOAD2 = 1
+    STORE = 2
+)
 func readFile(filepath string) ([]byte, os.Error) {
         f, e := os.Open(filepath, os.O_RDONLY, 0)
         if e != nil {
-                return "", e
+                return nil, e
         }
         defer f.Close()
 
         bqry, e := ioutil.ReadAll(f)
         if e != nil {
-                return "", e
+                return nil, e
         }
         return bqry, e
 }
@@ -37,7 +51,7 @@ func parse() ([]byte) {
 		os.Exit(0)
 	}
 
-	mem, e := readFile(file)
+	mem, e := readFile(*file)
 	if e != nil {
 		fmt.Fprintf(os.Stderr, "Could not read file: %s\n", e.String())
 		os.Exit(1)
@@ -48,22 +62,55 @@ func parse() ([]byte) {
 
 func New(memory []byte) (r *RPU) {
 	r = new(RPU)
-	r.Memory = make([]byte, 4096)
-	r.Memory[0:len(memory)] = memory[0:]
+	r.memory = make([]byte, 4096)
+    for i,v := range memory {
+        if i >= len(r.memory) {
+            break
+        }
+        r.memory[i] = v
+    }
+    r.ip = 0;
 	return
 }
 
-func (r *RPU) getOp() byte {
-	return (r.Memory[r.ip] >> 7) & 1
+func (r *RPU) GetOp() Op {
+	return (r.memory[r.ip] >> 7) & 1
 }
 
-func (r *RPU) getOperand() uint16  {
-	return ((r.Memory[r.ip] & 0x7F) << 8) | r.Memory[r.ip+1]
+func (r *RPU) GetPhase() Phase {
+    return r.IP % 3
+}
+
+func (r *RPU) GetOperand() uint16  {
+	return ((uint16(r.memory[r.ip]) & 0x7F) << 8) | uint16(r.memory[r.ip+1])
+}
+
+func (r *RPU) GetAddress(addr uint16) Bit {
+    return (m.memory[addr/8] >> (addr%8)) & 1
+}
+
+func (r *RPU) Next() {
+    r.ip++
+}
+
+func (r *RPU) Exec() {
+    switch r.getOp() {
+        case LOAD:
+            
+        case STORE:
+    }
+}
+
+func (r *RPU) GetIP() uint16 {
+    return r.ip
 }
 
 func main() {
 	memory := parse()
 
-	i := New(memory)
-	
+	m := New(memory)
+    for i:=0; i < 2048; i++ {
+        fmt.Printf("%02x: %x %04x\n", m.getIP(), m.getOp(), m.getOperand())
+        m.next()
+    }
 }
